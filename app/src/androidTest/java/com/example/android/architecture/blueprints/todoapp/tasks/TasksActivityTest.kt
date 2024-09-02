677f71b093277459eaa6be593576231a31ac781b
FAWRY\AbdulHameed.Mohamed <abdulhameed.mohamed@fawry.com>
2024-09-02 21:21:21 +0300

    make E2E Tests For TasksActivity.kt as a LargeTest But Its A Flaky Test.  Will Solve It Later Because Of DataBinding And Long Running Operation Sync With Espresso.

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivityTest.kt
@@ -0,0 +1,69 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers.not
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class TasksActivityTest {
+
+    private lateinit var repository: TasksRepository
+
+    @Before
+    fun init() {
+        repository = ServiceLocator.provideTasksRepository(getApplicationContext())
+        runBlocking {
+            repository.deleteAllTasks()
+        }
+    }
+
+
+    @Test
+    fun editTask() = runBlocking {
+
+        repository.saveTask(Task("TITLE1", "DESCRIPTION"))
+
+        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+
+        onView(withText("TITLE1")).perform(click())
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("TITLE1")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("DESCRIPTION")))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
+
+        onView(withId(R.id.edit_task_fab)).perform(click())
+        onView(withId(R.id.add_task_title_edit_text)).perform(replaceText("NEW TITLE"))
+        onView(withId(R.id.add_task_description_edit_text)).perform(replaceText("NEW DESCRIPTION"))
+        onView(withId(R.id.save_task_fab)).perform(click())
+
+        onView(withText("NEW TITLE")).check(matches(isDisplayed()))
+        onView(withText("TITLE1")).check(doesNotExist())
+
+        activityScenario.close()
+    }
+
+    @After
+    fun reset() {
+        ServiceLocator.resetRepository()
+    }
+}
